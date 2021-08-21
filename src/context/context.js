2093eea92452da6c6cbeccc6940881b1557cebab
unknown <azhar.khalid10@gmail.com>
2021-08-21 18:11:17 +0530

    search functionality added

--- src/context/context.js
+++ src/context/context.js
@@ -14,8 +14,67 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  //request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setisLoading] = useState(false);
+  //errors
+  const [error, setError] = useState({ show: false, msg: "" });
+  const searchGithubUser = async (user) => {
+    toggleError();
+    //SetLoading(true)
+    setisLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      //repos
+      axios(`${rootUrl}/users/${login}/repos? per_page=100`).then((response) =>
+        setRepos(response.data)
+      );
+      //followers
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      );
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+    checkRequests();
+    setisLoading(false);
+  };
+  // Check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          //throw an error
+          toggleError(true, "sorry, you have exceeded your hourly rate limit!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+  //error
+  useEffect(checkRequests, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
